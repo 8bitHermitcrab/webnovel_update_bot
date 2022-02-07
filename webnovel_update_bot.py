e73f3d20cdf370b2e662769e47344b6d5d67eb89
8bithermitcrab <kij517@naver.com>
2022-02-08 00:17:52 +0900

    webnovel_update

--- webnovel_update_bot.py
+++ webnovel_update_bot.py
@@ -1,7 +1,6 @@
 import requests, time
 from bs4 import BeautifulSoup
 
-
 url = 'https://ncode.syosetu.com/n2267be/'
 headers = {'User-Agent' : 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36'}
 
